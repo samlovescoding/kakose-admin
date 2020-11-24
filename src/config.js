0c7ea795104fdb7cd02f2fcc845ad126ed83cfaf
Sampan Verma <sampan.developer@gmail.com>
2020-11-24 12:42:12 +0530

    Updated config route

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  BACKEND_URL: "http://localhost:4000",
+  BACKEND_URL: "http://kakose-server.herokuapp.com",
   LOGIN_REDIRECT: "/bookings/today",
   GOOGLE_MAPS_API_KEY: "AIzaSyAcOzjNrOYtsleRMgXRNXZWPehh_dkKb4Y",
 };
