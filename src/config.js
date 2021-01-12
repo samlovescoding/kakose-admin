5b3370351317686da2e0eae7e92a1811fee4ee5c
Sampan Verma <sampan.developer@gmail.com>
2021-01-12 23:07:44 +0530

    Migration commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  BACKEND_URL: "http://kakose-server.herokuapp.com",
+  BACKEND_URL: "http://localhost:4000",
   LOGIN_REDIRECT: "/sheets",
   GOOGLE_MAPS_API_KEY: "AIzaSyAcOzjNrOYtsleRMgXRNXZWPehh_dkKb4Y",
 };
